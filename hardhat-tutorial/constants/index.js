630508cb153d03a33592c82c5f576738b314465f
Andrés Barros Rivas <andresbr92@gmail.com>
2022-11-13 19:33:41 +0100

    add constants

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x1e6Cd9D05E254a1Fbf638c16386abB87Ba9C1CC7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
